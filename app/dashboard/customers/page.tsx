69e9739fe272ef818e26ff2ec6911774f84458b4
Kevin-qiu68 <1291639721@qq.com>
2024-01-11 19:49:03 +0200

    v2.0

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,28 @@
-export default function Page(){
-    return <p>Customer Page</p>
+import { lusitana } from '@/app/ui/fonts';
+import { Suspense } from 'react';
+import Table from '@/app/ui/customers/table';
+import Search from '@/app/ui/search';
+import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
+
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+
+
+  return (
+    
+    <Table query={query} />
+  );
 }
